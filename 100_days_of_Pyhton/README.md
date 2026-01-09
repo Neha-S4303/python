9b358437db181932cca9a306d629d2a906b15459
Neha <nh.shekhawat@gmail.com>
2026-01-08 23:52:14 -0600

    first 2 days of the challenge

---
+++ 100_days_of_Pyhton/README.md
@@ -0,0 +1,12 @@
+##Day 1- 'Hello, World!'
+
+#Day 2: Variables
+Create the following variables of different types and print them.
+name: String
+age: Integer
+height: Floating-point number
+is_student: Boolean
+email: None (representing a null value)
+siblings: List
+address: Dictionary containing street and city
+
---
+++ 100_days_of_Pyhton/day_2_variables.py
@@ -0,0 +1,17 @@
+
+
+name = 'John'
+age = 32
+height = 5.8
+is_student = True
+email = None
+siblings = ["Jessica", "Jerry"]
+address = {"street": '123 Main St', "city": "California"}
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is_student:", is_student)
+print("Email:", email)
+print("Siblings:", siblings)
+print("Address :", address)
